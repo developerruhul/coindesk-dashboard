1b1d25dbba9737092da2652a3bc7461a4f767460
iamruhul <ruhulamin3482@gmail.com>
2018-05-11 15:03:59 +0600

    set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require("gulp");
+const del = require("del");
+const server = require("browser-sync").create();
+const htmlclean = require("gulp-htmlclean");
+const cleanCSS = require("gulp-clean-css");
+const sass = require("gulp-sass");
+const prefix = require("gulp-autoprefixer");
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// DEVELOPMENT
+gulp.task("sass", () => {
+  gulp
+    .src("src/scss/*.scss")
+    .pipe(sass().on("error", e => console.log(e)))
+    .pipe(gulp.dest("src/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["sass"], () => {
+  server.init({
+    server: {
+      baseDir: "src"
+    }
+  });
+
+  gulp.watch(["src/scss/**/*.scss"], ["sass"]);
+  gulp.watch("src/index.html").on("change", server.reload);
+});
+
+gulp.task("default", ["serve"]);
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// PRODUCTION
+gulp.task("dist:css", () => {
+  gulp
+    .src("src/scss/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("dist:html", () => {
+  gulp
+    .src("src/index.html")
+    .pipe(htmlclean())
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("dist:images", () =>
+  gulp.src("src/images/**").pipe(gulp.dest("dist/images/"))
+);
+
+gulp.task("build", ["dist:html", "dist:css", "dist:images"]);
