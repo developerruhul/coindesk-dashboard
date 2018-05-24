3550f6dd248f5343214cb8ad2c17358387960214
Muhammad Ruhul Amin <mramin3482@gmail.com>
2018-05-24 09:46:30 +0600

    create statistics chart with chartjs

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const htmlclean = require("gulp-htmlclean");
 const cleanCSS = require("gulp-clean-css");
 const sass = require("gulp-sass");
 const prefix = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // DEVELOPMENT
@@ -24,7 +25,7 @@ gulp.task("serve", ["sass"], () => {
   });
 
   gulp.watch(["src/scss/**/*.scss"], ["sass"]);
-  gulp.watch("src/index.html").on("change", server.reload);
+  gulp.watch(["src/index.html", "src/script/**"]).on("change", server.reload);
 });
 
 gulp.task("default", ["serve"]);
@@ -53,4 +54,11 @@ gulp.task("dist:images", () =>
   gulp.src("src/images/**").pipe(gulp.dest("dist/images/"))
 );
 
-gulp.task("build", ["dist:html", "dist:css", "dist:images"]);
+gulp.task("dist:js", () =>
+  gulp
+    .src("src/script/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/script/"))
+);
+
+gulp.task("build", ["dist:html", "dist:css", "dist:js", "dist:images"]);
---
+++ src/script/index.js
@@ -0,0 +1,93 @@
+var chart = document.getElementById("chart").getContext("2d"),
+  gradient = chart.createLinearGradient(0, 0, 0, 350);
+
+gradient.addColorStop(0, "rgba(0, 72, 253, 0.54)");
+gradient.addColorStop(0.5, "rgba(0, 72, 253, 0.24)");
+gradient.addColorStop(1, "rgba(255, 255, 255, 0.5)");
+
+Chart.defaults.global.defaultFontFamily = "Open Sans";
+
+var data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  datasets: [
+    {
+      label: "Income/Expence",
+      backgroundColor: gradient,
+      pointBackgroundColor: "white",
+      borderWidth: 3,
+      borderColor: "#205df9",
+      data: [5, 32, 13, 29, 36, 11, 36]
+    }
+  ]
+};
+
+var options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  animation: {
+    easing: "easeInOutQuad",
+    duration: 520
+  },
+  scales: {
+    xAxes: [
+      {
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          display: true,
+          fontStyle: "bold",
+          fontSize: 12
+        }
+      }
+    ],
+    yAxes: [
+      {
+        position: "right",
+        gridLines: {
+          color: "rgba(0, 0, 0, 0.05)",
+          lineWidth: 2,
+          zeroLineColor: "transparent"
+        },
+        ticks: {
+          display: true,
+          fontStyle: "bold",
+          fontSize: 12,
+          beginAtZero: true,
+          callback: function(value) {
+            return "$" + value + "k";
+          },
+          autoSkip: true,
+          maxTicksLimit: 5
+        }
+      }
+    ]
+  },
+  elements: {
+    line: {
+      tension: 0.4
+    }
+  },
+  legend: {
+    display: false
+  },
+  point: {
+    backgroundColor: "blue"
+  },
+  tooltips: {
+    titleFontFamily: "Open Sans",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    titleFontColor: "white",
+    caretSize: 0,
+    cornerRadius: 14,
+    xPadding: 10,
+    yPadding: 10
+  }
+};
+
+var chartInstance = new Chart(chart, {
+  type: "line",
+  data: data,
+  options: options
+});
